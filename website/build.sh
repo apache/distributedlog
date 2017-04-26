3469fc878295c4f63413d84b96a5d18f6890a109
Sijie Guo <sijie@apache.org>
2017-04-26 11:41:12 -0700

    Release 0.4.0-incubating
    Changes for 0.4.0-incubating
    
    - change the stable doc to 0.4.0-incubating
    - change latest version to 0.5.0-incubating-snapshot
    - add 0.4.0-incubating to the header
    - copied current docs to website/docs/0.4.0-incubating as the documents for release 0.4.0-incubating.
    
    Author: Sijie Guo <sijie@apache.org>
    
    Reviewers: Jia Zhai <zhaijia03@gmail.com>, Jia Zhai <zhaijia03@gmail.com>, Leigh Stewart <lstewart@apache.org>
    
    Closes #109 from sijie/sijie/release_0.4.0

--- website/build.sh
+++ website/build.sh
@@ -50,9 +50,6 @@ if [ ! -d "${DLOG_HOME}/website/docs" ]; then
   mkdir ${DLOG_HOME}/website/docs
 fi
 
-# Get the project version
-PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version 2> /dev/null | grep -Ev '(^\[|Download\w+:)')
-
 echo "Building the website to ${DEST_DIR}/content ..."
 
 # build the website
@@ -62,46 +59,56 @@ bundle exec jekyll build --destination ${DEST_DIR}/content --config _config.yml,
 
 echo "Built the website @ ${DEST_DIR}/content."
 
-echo "Building the documentation for version ${PROJECT_VERSION} ..."
+
 
 # build the documents
 
-if [[ ${PROJECT_VERSION} == *"SNAPSHOT"* ]]; then
-  # it is a snapshot version, built the docs into latest
-  DOC_SRC_HOME="${DLOG_HOME}/website/docs/latest"
-  DOC_DEST_HOME="${DEST_DIR}/content/docs/latest"
-else
-  # it is a release version, built the docs to release-version directory.
-  DOC_SRC_HOME="${DLOG_HOME}/website/docs/${PROJECT_VERSION}"
-  DOC_DEST_HOME="${DEST_DIR}/content/docs/${PROJECT_VERSION}"
-fi
+function build_docs() {
+  version=$1
+  tag=$2
 
-# link the doc source directory if necessary
-if [ ! -d "${DOC_SRC_HOME}" ]; then
-  ln -s ../../docs ${DOC_SRC_HOME} 
-fi
+  echo "Building the documentation for version ${version} ..."
 
-# create the doc dest directory
-mkdir -p ${DOC_DEST_HOME}
+  DOC_SRC_HOME="${DLOG_HOME}/website/docs/${version}"
+  DOC_DEST_HOME="${DEST_DIR}/content/docs/${version}"
 
-cd ${DOC_SRC_HOME}
+  cd ${DOC_SRC_HOME}
 
-bundle exec jekyll build --destination ${DOC_DEST_HOME} --config _config.yml,${OVERRIDED_CONFIG}
+  bundle exec jekyll build --destination ${DOC_DEST_HOME} --config _config.yml,${OVERRIDED_CONFIG}
+
+  if [ "$version" == "latest" ]; then
+    cd ${DLOG_HOME}
+    # create the api directory
+    mkdir -p ${DEST_DIR}/content/docs/latest/api/java
+    # build the javadoc
+    mvn -DskipTests clean package javadoc:aggregate \
+        -Ddoctitle="Apache DistributedLog for Java, version ${version}" \
+        -Dwindowtitle="Apache DistributedLog for Java, version ${version}" \
+        -Dmaven.javadoc.failOnError=false
+    # copy the built javadoc
+    cp -r ${DLOG_HOME}/target/site/apidocs/* ${DOC_DEST_HOME}/api/java
+  else
+    rm -r /tmp/distributedlog-${version}
+    git clone https://git-wip-us.apache.org/repos/asf/incubator-distributedlog.git /tmp/distributedlog-${version}
+    cd /tmp/distributedlog-${version}
+    git checkout $tag
+    # create the api directory
+    mkdir -p ${DEST_DIR}/content/docs/${version}/api/java
+    # build the javadoc
+    mvn -DskipTests clean package javadoc:aggregate \
+        -Ddoctitle="Apache DistributedLog for Java, version ${version}" \
+        -Dwindowtitle="Apache DistributedLog for Java, version ${version}" \
+        -Dmaven.javadoc.failOnError=false
+    # copy the built javadoc
+    cp -r /tmp/distributedlog-${version}/target/site/apidocs/* ${DOC_DEST_HOME}/api/java
+  fi
+  echo "Built the documentation for version ${version}."
+}
 
 # build the javadoc API
 
-cd ${DLOG_HOME}
-# create the api directory
-mkdir -p ${DEST_DIR}/content/docs/latest/api/java
-# build the javadoc
-mvn -DskipTests clean package javadoc:aggregate \
-    -Ddoctitle="Apache DistributedLog for Java, version ${PROJECT_VERSION}" \
-    -Dwindowtitle="Apache DistributedLog for Java, version ${PROJECT_VERSION}" \
-    -Dmaven.javadoc.failOnError=false
-# copy the built javadoc
-cp -r ${DLOG_HOME}/target/site/apidocs/* ${DOC_DEST_HOME}/api/java
-
-echo "Built the documentation for version ${PROJECT_VERSION}."
+build_docs "latest"
+build_docs "0.4.0-incubating" "v0.4.0-incubating-RC4_2.11"
 
 if [[ "${SERVE}" == "TRUE" ]]; then
   cd ${DLOG_HOME}/website
