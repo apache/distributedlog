32cceab2b349e27df70c9b8a26044ddd68b39866
Sijie Guo <sijie@apache.org>
2017-05-26 14:04:31 -0700

    fix merge script issue

--- scripts/dev/dl-merge-pr.py
+++ scripts/dev/dl-merge-pr.py
@@ -450,7 +450,7 @@ def get_reviewers(pr_num):
   for reviewer_id in reviewers_ids:
     username = None
     useremail = None
-    if reviewers[reviewer_id] is not None:
+    if reviewer_id in reviewers:
       reviewer = reviewers[reviewer_id]
       username = reviewer['name']
       useremail = reviewer['email']
